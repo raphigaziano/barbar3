a77bd7a489afdc28fffeb4d0538f2a3d325546b4
raphigaziano <r.gaziano@gmail.com>
2013-12-14 20:57:13 +0100

    moar settings tests

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -9,6 +9,13 @@ from utils import assets
 
 
 class TestSettings(unittest.TestCase):
+
+    def setUp(self):
+        self.clear_settings()
+
+    def clear_settings(self):
+        settings._SETTINGS_DICT = {}
+
     # TODO: use a dummy setting file to avoid hardcoding test values
     # TODO: check more types
     def test_settings(self):
@@ -17,6 +24,28 @@ class TestSettings(unittest.TestCase):
         self.assertEqual(80, settings.SCREEN_W)
         self.assertEqual(int, type(settings.SCREEN_W))
 
+    def test_missing_setting(self):
+        """ Missing (but not required) setting should default to None. """
+        self.assertEqual(None, settings._get_setting('MISSING_SETTING'))
+
+    def test_missing_required_setting(self):
+        """ Missing and required setting should raise an exception. """
+        self.assertRaises(
+            settings.InvalidSetting,
+            settings._get_setting,
+            ('MISSING_SETTING'),
+            required=True
+        )
+
+    def test_missing_default_setting(self):
+        """ Missing setting should default to its default value if specified. """
+        self.assertEqual(
+            'FOO', settings._get_setting('MISSING_SETTING', default='FOO')
+        )
+        self.clear_settings()
+        self.assertEqual(
+            666, settings._get_setting('MISSING_SETTING', default=666)
+        )
 
 class TestAssets(unittest.TestCase):
 
