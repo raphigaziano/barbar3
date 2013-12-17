b9aaeeb84c78235c8a337c0aa64f9909ab57548c
raphigaziano <r.gaziano@gmail.com>
2013-12-17 23:50:46 +0100

    map takes cells array directly in contructor

--- barbarian/map.py
+++ barbarian/map.py
@@ -9,6 +9,7 @@ Basic Map data structure.
 
 
 def dummy_generator():
+    """ Dummy map gen """
     from utils import rng
     return [
         rng.coin_flip() for i in range(80 * 40)
@@ -22,14 +23,13 @@ class Map(object):
 
     """ Basic Map object """
 
-    def __init__(self, width, height, generator):
-        # Placeholders w & h
+    def __init__(self, width, height, cells=None):
         self.w = width
         self.h = height
-        # Dummy map gen
-        self.cells = generator()
+        self.cells = cells or []
 
     def get_cell(self, x, y):
+        """ Get the cell at cartesian coordinates (x, y). """
         try:
             return self.cells[self._cartesian_to_idx(x, y)]
         except IndexError:
@@ -38,21 +38,30 @@ class Map(object):
             )
 
     def __iter__(self):
+        """
+        Iterator implentation.
+
+        return a 3 tuple of (x_position, y_postion, cell object).
+
+        """
         for i, c in enumerate(self.cells):
             x, y = self._idx_to_cartesian(i)
             yield x, y, c
         raise StopIteration
 
     def slice(self, x, y, w, h):
+        """ Return a submap object, which rect is defined by x, y, w & h. """
+        # Dummy stub.
         cells = [0, 0, 0]
-        return self.__class__(w, h, lambda: cells)
+        return self.__class__(w, h, cells)
 
     ### Internal Utils ###
     ######################
 
     def _idx_to_cartesian(self, idx):
+        """ Convert internal array index to cartesian coordinates. """
         return idx % self.w, idx / self.w
 
     def _cartesian_to_idx(self, x, y):
+        """ Convert cartesian coordinates to internal array index. """
         return x + (y * self.w)
-
--- tests/test_map.py
+++ tests/test_map.py
@@ -13,18 +13,18 @@ class TestMap(unittest.TestCase):
 
     def test_get_cell(self):
         """ Accessing map cells """
-        m = map.Map(10, 10, lambda: [1] * 100)
+        m = map.Map(10, 10, [1] * 100)
         [self.assertEqual(
             1, m.get_cell(x, y)
         ) for y in range(10) for x in range(10)]
-        m = map.Map(10, 10, lambda: [0] * 100)
+        m = map.Map(10, 10, [0] * 100)
         [self.assertEqual(
             0, m.get_cell(x, y)
         ) for y in range(10) for x in range(10)]
 
     def test_invalid_map_location(self):
         """ Trying to access an out of bounds cell should raise an exception """
-        m = map.Map(2, 8, lambda: [1] * 10)
+        m = map.Map(2, 8, [1] * 10)
         self.assertRaises(
             map.OutOfBoundMapError, m.get_cell, *(10, 10)
         )
@@ -37,7 +37,7 @@ class TestMap(unittest.TestCase):
 
     def test_iter(self):
         """ Iterating directly over Map objects """
-        m = map.Map(3, 3, lambda: range(9))
+        m = map.Map(3, 3, range(9))
         dummy_map = [
             (0, 3, 6),
             (1, 4, 7),
@@ -48,7 +48,7 @@ class TestMap(unittest.TestCase):
 
     def test_helper_cart_to_idx(self):
         """ Converting cartesian coodrinates to array index """
-        m = map.Map(10, 10, lambda: [1] * 100)
+        m = map.Map(10, 10, [1] * 100)
         self.assertEqual(0,  m._cartesian_to_idx(0, 0))
         self.assertEqual(1,  m._cartesian_to_idx(1, 0))
         self.assertEqual(9,  m._cartesian_to_idx(9, 0))
@@ -64,7 +64,7 @@ class TestMap(unittest.TestCase):
 
     def test_helper_idx_to_cart(self):
         """ Converting array index to cartesian coordinates """
-        m = map.Map(10, 10, lambda: [1] * 100)
+        m = map.Map(10, 10, [1] * 100)
         self.assertEqual((0, 0), m._idx_to_cartesian(0))
         self.assertEqual((1, 0), m._idx_to_cartesian(1))
         self.assertEqual((9, 0), m._idx_to_cartesian(9))
@@ -76,7 +76,7 @@ class TestMap(unittest.TestCase):
 
     def test_helpers_round_trip(self):
         """ From indexes to cartesian and back & vice versa """
-        m = map.Map(10, 10, lambda: [1] * 100)
+        m = map.Map(10, 10, [1] * 100)
         [self.assertEqual(
             i, m._cartesian_to_idx(*m._idx_to_cartesian(i))
         ) for i in range(100)]
@@ -87,5 +87,5 @@ class TestMap(unittest.TestCase):
     def test_slice(self):
         """ GAGAGA """
         # Dummy failing test
-        m = map.Map(10, 10, lambda: [1] * 100)
+        m = map.Map(10, 10, [1] * 100)
         self.assertListEqual([0, 0, 0], m.slice(10, 10, 10, 10).cells)
