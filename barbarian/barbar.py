a05065d4da30f09a8f45414c55e65389595ee666
raphigaziano <r.gaziano@gmail.com>
2014-02-02 18:43:06 +0100

    pudb tweak

--- barbarian/barbar.py
+++ barbarian/barbar.py
@@ -7,6 +7,13 @@ Game entry point.
 
 """
 # pylint: disable=E1101
+try:
+    # TODO: check a DEBUG_MODE config var to decide whether we set the debugger
+    # or not.
+    from pudb import set_interrupt_handler
+    set_interrupt_handler()
+except ImportError:
+    pass
 
 from barbarian.gamestates import StateManager, InitState
 
