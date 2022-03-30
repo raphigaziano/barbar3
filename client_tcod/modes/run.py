raphigaziano/barbar3

commit 19ec459f4a4a67fa1e643b254c6cd67fff44c4e5

Cleanup: get rid of client requests.
Those aren't necessary anymore now that modes store a ref to their owner
gamemode.

client_tcod/modes/run.py
@@ -20,7 +20,7 @@ class RunMode(BaseGameMode):
     __gamelog = []
     __bloodstains = []
 
-    def cmd_open_door(self, _):
+    def open_door(self):
         surrounding_doors = [
             d for d in self.get_surrounding_entities(
                 self.client.gamestate.props, 'door')
@@ -29,7 +29,7 @@ class RunMode(BaseGameMode):
         if not self._open_or_close_door(surrounding_doors):
             self.log_msg('There are no closed doors around you.')
 
-    def cmd_close_door(self, _):
+    def close_door(self):
         surrounding_doors = [
             d for d in self.get_surrounding_entities(
                 self.client.gamestate.props, 'door')
@@ -82,11 +82,11 @@ class RunMode(BaseGameMode):
 
         return [e for e in entity_list if predicate(e)]
 
-    def cmd_move_r(self, data):
+    def move_r(self, data):
         """ Move repeatedly until an ennemy is spotted """
         self.push(AutoRunMode(action_name='move', action_data=data))
 
-    def cmd_autoxplore(self, _):
+    def autoxplore(self):
         """ Same as above, but with an autoexploring move """
         self.push(AutoRunMode(action_name='xplore'))
 

client_tcod/client.py
@@ -71,28 +71,6 @@ class BarbarClient:
         self.response = self.con.send(r)
         self.process_response()
 
-    def process_request(self, r):
-        """
-        Preprocess a request before sending it.
-
-        Some requests (called `client` requests) will be handled
-        by the client and not sent to the game: this is where
-        we handle this distinction.
-
-        """
-        if r['type'] in ('ACT', 'GET', 'SET'):
-            self.send_request(r)
-        # Special case for client requests
-        else:
-            # First see if the client has a hendler for this
-            cmd = r['data']['cmd']
-            handler = getattr(self, f"cmd_{cmd}", None)
-            if handler:
-                handler(r['data'])
-            # If not, then delegate to the current mode
-            else:
-                self.current_mode.process_request(r)
-
     def process_response(self):
         """
         Preprocess a response (mainly by nabbing its gamesate)
@@ -120,7 +98,7 @@ class BarbarClient:
             self.render()
             request = self.game_modes.update()
             if request:
-                self.process_request(request)
+                self.send_request(request)
             self.clock.sync()
 
         return self.shutdown()

client_tcod/event_handlers.py
@@ -58,23 +58,19 @@ class DebugEventsMixin:
 
         if (e.mod & tcod.event.KMOD_LALT and e.sym == tcod.event.K_x):
             v = not constants.SHOW_UNEXPLORED_CELLS
-            return Request.client(
-                'setvar_g', {'key': 'SHOW_UNEXPLORED_CELLS', 'val': v})
+            return self.mode.setvar_g('SHOW_UNEXPLORED_CELLS', v)
 
         if (e.mod & tcod.event.KMOD_LALT and e.sym == tcod.event.K_p):
             v = not constants.SHOW_PATH_INFO
-            return Request.client(
-                'setvar_g', {'key': 'SHOW_PATH_INFO', 'val': v})
+            return self.mode.setvar_g('SHOW_PATH_INFO', v)
 
         if (e.mod & tcod.event.KMOD_LALT and e.sym == tcod.event.K_v):
             v = not constants.SHOW_SPAWN_ZONES
-            return Request.client(
-                'setvar_g', {'key': 'SHOW_SPAWN_ZONES', 'val': v})
+            return self.mode.setvar_g('SHOW_SPAWN_ZONES', v)
 
         if (e.mod & tcod.event.KMOD_LALT and e.sym == tcod.event.K_f):
             v = not constants.IGNORE_FOV
-            return Request.client(
-                'setvar_g', {'key': 'IGNORE_FOV', 'val': v})
+            return self.mode.setvar_g('IGNORE_FOV', v)
 
 
 class RunEventHandler(DebugEventsMixin, BaseEventHandler):
@@ -90,7 +86,7 @@ class RunEventHandler(DebugEventsMixin, BaseEventHandler):
 
         if e.sym in MOVE_KEYS:
             if e.mod & tcod.event.KMOD_LSHIFT:
-                return Request.client('move_r', {'dir': MOVE_KEYS[e.sym]})
+                return self.mode.move_r({'dir': MOVE_KEYS[e.sym]})
             else:
                 return Request.action('move', {'dir': MOVE_KEYS[e.sym]})
 
@@ -103,9 +99,9 @@ class RunEventHandler(DebugEventsMixin, BaseEventHandler):
             return self.mode.drop_item()
 
         if (e.mod & tcod.event.KMOD_LCTRL and e.sym == tcod.event.K_o):
-            return Request.client('open_door')
+            return self.mode.open_door()
         if (e.mod & tcod.event.KMOD_LCTRL and e.sym == tcod.event.K_c):
-            return Request.client('close_door')
+            return self.mode.close_door()
 
         if e.sym == tcod.event.K_LESS:
             if e.mod & tcod.event.KMOD_LSHIFT:
@@ -115,7 +111,7 @@ class RunEventHandler(DebugEventsMixin, BaseEventHandler):
             return Request.action('use_prop', {'use_key': use_key})
 
         if (e.mod & tcod.event.KMOD_LCTRL and e.sym == tcod.event.K_x):
-            return Request.client('autoxplore')
+            return self.mode.autoxplore()
 
         if e.sym == tcod.event.K_i:
             self.mode.show_inventory()
@@ -133,7 +129,7 @@ class DbgMapEventHandler(DebugEventsMixin, BaseEventHandler):
 
         # Call it *before* super().debug_cmds,to shadow its Alt-r event
         if (e.mod & tcod.event.KMOD_LALT and e.sym == tcod.event.K_r):
-            return Request.client('setvar', {'key': 'mapgen_index', 'val': 0})
+            return self.mode.setvar('mapgen_index', 0)
 
         if r := super().debug_events(e):
             return r
@@ -147,7 +143,7 @@ class GameOverEventHandler(BaseEventHandler):
             return r
 
         if e.sym == tcod.event.K_n:
-            return Request.client('start')
+            return self.mode.client.start(None)
 
 
 class BaseUIModalEventHandler(BaseEventHandler):

client_tcod/modes/base.py
@@ -148,26 +148,16 @@ class BaseGameMode:
 
     # --- Mode run ---
 
-    def cmd_setvar(self, data):
-        k, v = data['key'], data['val']
+    def setvar(self, k, v):
         setattr(self, k, v)
         print(f"{k}: {getattr(self, k)}")
 
-    def cmd_setvar_g(self, data):
-        k, v = data['key'], data['val']
+    def setvar_g(self, k, v):
         setattr(constants, k, v)
         print(f"{k}: {getattr(constants, k)}")
 
     def update(self):
         return self.event_handler.handle(self.client.context)
-
-    def process_request(self, r):
-        cmd = r['data'].pop('cmd')
-        handler = getattr(self, f"cmd_{cmd}", None)
-        if handler:
-            handler(r['data'])
-        else:
-            print(f'No handler for command type {cmd}')
 
     def process_response(self, r):
         pass    # No-op

client_tcod/nw.py
@@ -31,12 +31,6 @@ class Request(dict):
         d = {'key': key, 'val': val}
         return cls(session_key=cls.session_key, type='SET', data=d)
 
-    @classmethod
-    def client(cls, cmd, data=None):
-        d = data or {}
-        d['cmd'] = cmd
-        return cls(session_key=cls.session_key, type='CLIENT', data=d)
-
 
 class Response(SimpleNamespace):
 

client_tcod/render.py
@@ -297,7 +297,7 @@ class TcodRenderer:
                 x, y = idx_to_c(idx, gamestate.map['width'])
                 col_idx = cval // 10
                 color = (
-                    self._path_colors[col_idx] if col_idx < len(self._path_colors) 
+                    self._path_colors[col_idx] if col_idx < len(self._path_colors)
                     else tcod.white)
                 char = str(cval)[-1]
                 self.hud_console.print(x, y, char, color, bg=tcod.black)
@@ -314,7 +314,7 @@ class TcodRenderer:
                 else:
                     color = self._zone_colors[i]
                 for x, y in zone:
-                    self.hud_console.print(x, y, str(i), color)
+                    self.hud_console.print(x, y, str(i), color, bg=tcod.black)
 
     def render_hud(self, gamestate):
         self.hud_console.clear(bg=TRANS_COLOR)

commit af444e0543645959994cf81e6bbd59df85cc2d9b

Bufgix - Autorun mode was broken since 19ec459f
Tried to call deleted proces_request method when receiving input.

client_tcod/modes/run.py
@@ -212,7 +212,6 @@ class AutoRunMode(RunMode):
         request = super().update()
         if request:
             self.pop()
-            return self.client.process_request(request)
 
     def process_game_events(self, game_events):
         for ge in game_events: